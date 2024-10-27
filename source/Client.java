47d40108eb287b221033556cc7cdeb20784b3d64
Julia Trinh <juliatrinh2002@gmail.com>
2024-10-27 15:24:33 -0400

    added schedule in Client class

--- source/Client.java
+++ source/Client.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Client extends RegisteredUser{
     private int age;
     private List<Booking> bookings;
+    private Schedule schedule; // TO-DO: add schedule initialization
 
     public Client(String name, String phoneNumber, int age){
         super(name, phoneNumber);
@@ -13,6 +14,8 @@ public class Client extends RegisteredUser{
 
     public void addBooking(Offering offering){
         if(offering.getIsAvailableToPublic()){
+
+            //TO-DO: check Client's schedule
             Booking booking = new Booking(this, offering);
             bookings.add(booking);
             offering.addBooking(booking);
