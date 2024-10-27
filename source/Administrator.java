b594ec4fb339b9e77f2c29651cdbeb414922b151
Angewoah <areoligio@gmail.com>
2024-10-26 23:56:01 -0400

    minor comments

--- source/Administrator.java
+++ source/Administrator.java
@@ -5,4 +5,10 @@ public class Administrator extends RegisteredUser{
     public Administrator(String name, String phoneNumber){
         super(name, phoneNumber);
     }
+
+    public void createOffering(){
+        //TODO (?)
+        //needs connection to organization
+        //maybe should have connection to organizations catalog to manage it
+    }
 }
